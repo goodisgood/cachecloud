79d3eefb523fcfd65917e1c31d0abc5586c3c440
“goodgoodisgood <jun3219@gmail.com>
2018-05-19 17:26:34 +0800

    修改机器信息收集及配置信息

--- cachecloud-open-web/src/main/java/com/sohu/cache/schedule/jobs/ServerJob.java
+++ cachecloud-open-web/src/main/java/com/sohu/cache/schedule/jobs/ServerJob.java
@@ -14,19 +14,24 @@ import com.sohu.cache.util.ConstUtils;
  */
 public class ServerJob extends CacheBaseJob {
 
-    @Override
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -3781713370092431607L;
+
+	@Override
     public void action(JobExecutionContext context) {
-//        try {
-//            JobDataMap dataMap =  context.getMergedJobDataMap();
-//            SchedulerContext schedulerContext = context.getScheduler().getContext();
-//            ApplicationContext applicationContext = (ApplicationContext)schedulerContext.get(APPLICATION_CONTEXT_KEY);
-//            ServerStatusCollector serverStatusCollector = applicationContext.getBean("serverStatusCollector", ServerStatusCollector.class);
-//            String ip = dataMap.getString(ConstUtils.HOST_KEY);
-//            serverStatusCollector.fetchServerStatus(ip);
-//        } catch (SchedulerException e) {
-//            logger.error(e.getMessage(), e);
-//        } catch (Exception e) {
-//            logger.error(e.getMessage(), e);
-//        }
+        try {
+            JobDataMap dataMap =  context.getMergedJobDataMap();
+            SchedulerContext schedulerContext = context.getScheduler().getContext();
+            ApplicationContext applicationContext = (ApplicationContext)schedulerContext.get(APPLICATION_CONTEXT_KEY);
+            ServerStatusCollector serverStatusCollector = applicationContext.getBean("serverStatusCollector", ServerStatusCollector.class);
+            String ip = dataMap.getString(ConstUtils.HOST_KEY);
+            serverStatusCollector.fetchServerStatus(ip);
+        } catch (SchedulerException e) {
+            logger.error(e.getMessage(), e);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
     }
 }
